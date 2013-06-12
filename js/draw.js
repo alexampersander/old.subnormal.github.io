625badc32fa89b47ec8b6fb96962847d3e492e30
Alex Taylor <awesumalex117@gmail.com>
2013-06-12 04:48:19 -0700

    Finally finished, and I'm done for now.

--- js/draw.js
+++ js/draw.js
@@ -1,23 +1,26 @@
-var el = document.getElementById("main"),
-    two = new Two({ 
-        fullscreen: true
-    });
- 
-two.appendTo(el);
+var elem = document.getElementById('draw-animation');
+var two = new Two({ width: 285, height: 200 }).appendTo(elem);
 
-// two has convenience methods to create shapes.
-var circle = two.makeCircle(72, 100, 50);
-var rect = two.makeRectangle(213, 100, 100, 100);
-
-// The object returned has many stylable properties:
+var circle = two.makeCircle(-70, 0, 50);
+var rect = two.makeRectangle(70, 0, 100, 100);
 circle.fill = '#FF8000';
-circle.stroke = 'orangered'; // Accepts all valid css color
-circle.linewidth = 5;
+rect.fill = 'rgba(0, 200, 255, 0.75)';
 
-rect.fill = 'rgb(0, 200, 255)';
-rect.opacity = 0.75;
-rect.noStroke();
+var group = two.makeGroup(circle, rect);
+group.translation.set(two.width / 2, two.height / 2);
+group.scale = 0;
+group.noStroke();
 
-// Don't forget to tell two to render everything
-// to the screen
-two.update();
+/* Bind a function to scale and rotate the group to the
+animation loop. */
+two.bind('update', function(frameCount) {
+	/* Called everytime two.update() is called.
+	   Effectively 60 times per second.
+	   In case you want this to loop:
+	if (group.scale > 0.9999) {
+		group.scale = group.rotation = 0;
+	}*/
+	var t = (1 - group.scale) * 0.125;
+	group.scale +=t;
+	group.rotation += t * 4 * Math.PI;
+}).play();
