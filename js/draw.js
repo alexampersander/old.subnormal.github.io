2976867a85621e5faada7462d8930dfced9a7504
Alex Taylor <awesumalex117@gmail.com>
2013-07-16 00:35:35 -0700

    Beautification!
    Aww yeah.

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,8 @@
 var elem = document.getElementById('draw-animation');
-var two = new Two({ width: 285, height: 250 }).appendTo(elem);
+var two = new Two({
+    width: 285,
+    height: 250
+}).appendTo(elem);
 
 var circle = two.makeCircle(-71, 0, 50);
 var rect = two.makeRectangle(70, 0, 100, 100);
@@ -13,14 +16,14 @@ group.noStroke();
 
 /* Bind a function to scale and rotate the group to the
 animation loop. */
-two.bind('update', function(frameCount) {
-	/* Called everytime two.update() is called.
+two.bind('update', function (frameCount) {
+    /* Called everytime two.update() is called.
 	Effectively 60 times per second.
 	In case you want this to loop:
 	if (group.scale > 0.9999) {
 		group.scale = group.rotation = 0;
 	}*/
-	var t = (1 - group.scale) * 0.125;
-	group.scale +=t;
-	group.rotation += t * 4 * Math.PI;
+    var t = (1 - group.scale) * 0.125;
+    group.scale += t;
+    group.rotation += t * 4 * Math.PI;
 }).play();
