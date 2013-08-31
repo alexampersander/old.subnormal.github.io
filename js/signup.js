2bf39d1be273c692722daeb511a0ce068b16837b
Alex Taylor <awesumalex117@gmail.com>
2013-08-30 20:22:30 -0700

    Work in progress

---
+++ js/signup.js
@@ -0,0 +1,9 @@
+// Intializing Parse
+Parse.initialize("Rct4o5JxkkksTyCOiJVutTNqJelNFxSvXlpI8f47", "uShtb5i3wDsVnLYYXHS7UdE86thIAmrwVQLHzQGn");
+
+// Log In and Sign Up
+function register() {
+    // Get the values from login page
+    var $username = $("#username"),
+        $password = $("#password"),
+}
