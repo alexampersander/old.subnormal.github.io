01adaf8d62749c29f743c5732f81381d00dc7158
Alex Taylor <awesumalex117@gmail.com>
2013-09-09 06:05:58 -0700

    FINALLY! signUp functional! Next is logIn!

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,32 @@
 // Intializing Parse
 Parse.initialize("Rct4o5JxkkksTyCOiJVutTNqJelNFxSvXlpI8f47", "uShtb5i3wDsVnLYYXHS7UdE86thIAmrwVQLHzQGn");
 
+var zindex = 100;
 // Validates that all entered information is correct
 function validateStuff() {
+    var userRegex = /^[\w\.@]{6,30}$/;
+    var validName = document.getElementById('username').value.match(userRegex);
+    if (!validName) {
+        $('#badname').removeClass("hidden");
+        $('#shortpass').addClass("hidden");
+        $('#confpass').addClass("hidden");
+        $('#wrongemail').addClass("hidden");
+        return false;
+    }
     var y=document.forms["signup"]["password"].value;
     var y2=document.forms["signup"]["password2"].value;
     if (y.length < 6) {
         $('#shortpass').removeClass("hidden");
+        $('#badname').addClass("hidden");
+        $('#confpass').addClass("hidden");
+        $('#wrongemail').addClass("hidden");
         return false;
     }
     if (y != y2) {
         $('#confpass').removeClass("hidden");
+        $('#badname').addClass("hidden");
+        $('#shortpass').addClass("hidden");
+        $('#wrongemail').addClass("hidden");
         return false;
     }
     var x=document.forms["signup"]["email"].value;
@@ -18,6 +34,9 @@ function validateStuff() {
     var dotpos=x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
         $('#wrongemail').removeClass("hidden");
+        $('#badname').addClass("hidden");
+        $('#shortpass').addClass("hidden");
+        $('#confpass').addClass("hidden");
         return false;
     } else {return true;}
 }
@@ -34,18 +53,19 @@ function register() {
         password2 = $password.val(),
         email = $email.val();
 
+    var lowered = username.toLowerCase();
     var encryptPass = md5(password);
-    alert(encryptPass);
 
     var user = new Parse.User();
-    user.set("username", username);
+    user.set("username", lowered);
+    user.set("preferredname", username);
     user.set("password", encryptPass);
     user.set("email", email);
 
     user.signUp(null, {
         success: function(user) {
             //Login user
-            /*Parse.User.logIn(username, encryptPass {
+            /*user.logIn(lowered, encryptPass {
                 success: function(user) {
                     return true;
                 },
@@ -54,7 +74,7 @@ function register() {
                     return false;
                 }
             });*/
-            console.log("Signed up!");
+            return true;
         },
         error: function(user, error) {
             if (error.code == 202) {
@@ -74,6 +94,5 @@ function verify() {
     var truth = validateStuff();
     if (truth) {
         register();
-        return true;
     } else {return false;}
 }
